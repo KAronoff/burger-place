8b0336543e112df6ec113b0c359c6ccc640b6142
Kit Aronoff <dreamnmusic@gmail.com>
2019-05-06 09:59:17 -0400

    Finsihed the seeds and schema, then created the connection.js for the mysql.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burgerName) VALUES ('Veggie Delux');
+INSERT INTO burgers (burgerName) VALUES ('Vegan');
+INSERT INTO burgers (burgerName, eaten) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (burgerName, eaten) VALUES ('BBQ Burger', true);
+INSERT INTO burgers (burgerName, eaten) VALUES ('Freedom Burger', true);
+INSERT INTO burgers (burgerName) VALUES ('Tomato Burger');
